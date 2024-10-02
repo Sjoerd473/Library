d68f414d2284c94513ba3ed3aee7a54c3e1b622c
Sjoerd473 <sjoerdbrussee@hotmail.com>
2024-10-02 15:27:16 +0200

    added html skeleton, styling for cards, added function for creating a Book, function for adding a book

---
+++ script.js
@@ -0,0 +1,105 @@
+let bookOne = {
+    title: 'BookTitle',
+    author: 'Writer',
+    genre: 'Exciting',
+    pages: '50000',
+    status: 'unread'
+}
+
+let bookTwo = {
+    title: 'BookTitle',
+    author: 'Writer',
+    genre: 'Exciting',
+    pages: '50000',
+    status: 'unread'
+}
+
+let bookThree = {
+    title: 'BookTitle',
+    author: 'Writer',
+    genre: 'Exciting',
+    pages: '50000',
+    status: 'unread'
+}
+
+let bookFour = {
+    title: 'BookTitle',
+    author: 'Writer',
+    genre: 'Exciting',
+    pages: '50000',
+    status: 'read'
+}
+
+const myLibrary = [bookOne, bookTwo, bookThree, bookFour];
+
+function Book(title, author, genre, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.pages = pages;
+    this.status = status;
+}
+
+function addBookToLibrary(array) {
+    let container = document.getElementById('cards');
+    array.forEach((book) => {
+        let card = document.createElement('div');
+        container.appendChild(card);
+        let parent = document.createElement('ul');
+        card.appendChild(parent);
+
+        let delButton = document.createElement('button');
+        let readButton = document.createElement('button');
+        let cardButtons = document.createElement('div');
+
+        delButton.textContent = 'Delete';
+        if (book.status === 'unread') {
+            readButton.textContent = 'Read'
+        } else { readButton.textContent = 'Unread' }
+        card.appendChild(cardButtons)
+        cardButtons.appendChild(delButton)
+        cardButtons.appendChild(readButton);
+
+        let itemOne = document.createElement('li')
+        let itemTwo = document.createElement('li')
+        let itemThree = document.createElement('li')
+        let itemFour = document.createElement('li')
+        let itemFive = document.createElement('li')
+
+        itemOne.textContent = 'Title: ' + book.title;
+        parent.appendChild(itemOne);
+
+        itemTwo.textContent = 'Author: ' + book.author;
+        parent.appendChild(itemTwo);
+
+        itemThree.textContent = 'Genre: ' + book.genre;
+        parent.appendChild(itemThree);
+
+        itemFour.textContent = 'Number of pages: ' + book.pages;
+        parent.appendChild(itemFour);
+
+        itemFive.textContent = 'Read or unread? ' + book.status;
+        parent.appendChild(itemFive);
+
+    })
+}
+
+const dialog = document.querySelector('dialog');
+const closeButton = document.querySelector("dialog button");
+
+bookButton.addEventListener("click", () => {
+    dialog.showModal();
+  });
+
+  closeButton.addEventListener("click", () => {
+    dialog.close();
+  });
+
+  const submitButton = document.querySelector('#confirm')
+
+  submitButton.addEventListener('click',(event) => {
+   let title = document.querySelector('input[name="book_title"]').value
+   console.log(title)
+    console.log(event);
+    event.preventDefault();
+  })
